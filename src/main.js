7ccb6d76facddf0637a956aea8e2f0fe1d7bbd2a
Bogdan Pavlov <bogdanpavlov@MacBook-Pro-Bogdan.local>
2017-03-02 18:59:01 +0300

    this end

--- src/main.js
+++ src/main.js
@@ -10,10 +10,8 @@ LocalStorage.prototype.sync = function(data) {
 	localStorage.setItem(this.key, JSON.stringify(data))
 }
 
-let storage = new LocalStorage('library$1');
-
 function Library(options) {
-	this.storage = options.storage;
+ 	this.storage = options.storage;
 	this.list = this.storage.fetch();
 	this.collectionNodes(options);
 	this.bindEvents();
@@ -26,47 +24,50 @@ Library.prototype.collectionNodes = function (options) {
 	this.nameBook = document.getElementById(options.nameBook);
 	this.numberPages = document.getElementById(options.numberPages);
 	this.btnSubmit = document.getElementById(options.btnSubmit);
+	this.form = document.getElementById(options.form);
+	this.hiddenID = document.getElementById(options.hiddenID);
 }
 Library.prototype.bindEvents = function() {
 	let self = this;
 
-	self.btnSubmit.addEventListener('click', function(e) {
+	self.form.addEventListener('submit', function(e) {
 		e.preventDefault();
 
-		let valuesInputs = {
-			id: Date.now(),
-			author: self.author.value,
-			date: self.date.value,
-			name: self.nameBook.value,
-			pages: self.numberPages.value
-		};
+		if (!self.btnSubmit.classList.contains('update')) {
+			let valuesInputs = {
+				id: Date.now(),
+				author: self.author.value,
+				date: self.date.value,
+				name: self.nameBook.value,
+				pages: self.numberPages.value
+			};
 
-		self.add(valuesInputs);
+			self.form.reset()
+			self.add(valuesInputs);
+		} else {
+			self.btnSubmit.classList.remove('update');
+			self.btnSubmit.textContent = 'Publish';
+			self.edited(self.hiddenID.value)
+			self.form.reset()
+		}
 
-	})
-
-	let btn = document.querySelector('#updateBtn');
-	btn.addEventListener('click', function(e) {
-		e.preventDefault();
-		self.edited(document.querySelector('#dataID').value)
-	})
-
-
+	});
 }
 Library.prototype.createElement = function(index) {
 
 	let html = `<li class="list-item" data-id="${this.list[index].id}">
-					<h2>Автор: ${this.list[index].author}</h2>
-					<p>Название книги: ${this.list[index].name}</p>
-					<div class="btn-group">
-						<button type="button" class="btn btn-default removed">Remove</button>
-						<button type="button" class="btn btn-default edited">Edit</button>
+					<h4>Автор: ${this.list[index].author}</h4>
+					<p>Название: ${this.list[index].name}</p>
+					<div class="btn-items">
+						<button type="button" class="btn btn-danger removed">Remove</button>
+						<button type="button" class="btn btn-warning edited">Edit</button>
 					</div>
 				</li>`;
 
 	this.container.innerHTML += html;
 
-	let removeBtn = document.getElementsByClassName("removed");
+	let removeBtn = document.getElementsByClassName("removed"),
+		editBtn = document.getElementsByClassName('edited');
 
 	for (let i of removeBtn) {
 		i.addEventListener('click', function(e) {
@@ -74,14 +75,14 @@ Library.prototype.createElement = function(index) {
 		}.bind(this))
 	}
 
-	let editBtn = document.getElementsByClassName('edited');
-
 	for (let el of editBtn) {
 		el.addEventListener('click', function(e) {
+			this.btnSubmit.classList.add('update');
+			this.btnSubmit.textContent = 'Update';
+
 			this.update(e.target.closest('li'))
 		}.bind(this))
 	}
-
 }
 Library.prototype.edited = function(dataID) {
 
@@ -94,8 +95,9 @@ Library.prototype.edited = function(dataID) {
 				name: this.nameBook.value,
 				pages: this.numberPages.value
 			}
+
 			this.container.children[i].children[0].textContent = `Автор: ${this.list[i].author}`;
-			this.container.children[i].children[1].textContent = `Название книги: ${this.list[i].name}`;
+			this.container.children[i].children[1].textContent = `Название: ${this.list[i].name}`;
 			this.storage.sync(this.list);
 		}
 	}.bind(this))
@@ -106,21 +108,14 @@ Library.prototype.update = function(btnID) {
 
 	this.list.map(function(el, i) {
 		if (this.list[i].id == attrID) {
-			console.log(11)
 			let input = document.querySelector('#dataID')
 			input.value = attrID;
 			this.author.value = this.list[i].author;
 			this.date.value = this.list[i].date;
 			this.nameBook.value = this.list[i].name;
 			this.numberPages.value = this.list[i].pages;
-
 		}
 	}.bind(this))
-
-	let btn = document.querySelector('#updateBtn');
-	btn.addEventListener('click', function(e) {
-		e.preventDefault();
-	})
 }
 Library.prototype.render = function() {
 	this.list.map(function(el,i) {
@@ -145,6 +140,8 @@ Library.prototype.delete = function(btnTarget) {
 	this.storage.sync(this.list)
 }
 
+let storage = new LocalStorage('library$1');
+
 let library = new Library({
 	lists: 'library-items',
 	author: 'author',
@@ -152,5 +149,7 @@ let library = new Library({
 	nameBook: 'nameABook',
 	numberPages: 'numberPages',
 	btnSubmit: 'pushBtn',
+	form: 'form',
+	hiddenID: 'dataID',
 	storage: storage
 })
